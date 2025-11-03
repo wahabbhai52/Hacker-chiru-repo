250aac61b22bda5706fe8eaebe4323cf487b3ed0
wahabbhai52 <buddhimourya7@gmail.com>
2025-11-03 22:25:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.environ.get("API_HASH", "dbd8f5af56b0f6e16327c20a84eece99")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://fibegi:8oV4fjNNVasSfcoY@cluster0.jp8thup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 #OWNER = int(os.environ.get("OWNER", 7660860610))
 
